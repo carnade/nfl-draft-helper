f0f182db73d058b5acf939de83fc1d840ab70f4b
Andreas Bjuvefors <lb.andreas@gmail.com>
2025-05-04 00:45:56 +0200

    More work on draftGrid

--- src/DraftModal.js
+++ src/DraftModal.js
@@ -144,7 +144,7 @@ function DraftModal({ league, draftId, onClose, userId }) {
     setPlayerResults(orderedResults);
 
     // Log the counts per team
-  }, [isGoatActive, picks, draftType, calculateGoatValues, qbCount]);
+  }, [isGoatActive, picks, draftType, calculateGoatValues]);
 
   useEffect(() => {
     if (isGoatActive) {
