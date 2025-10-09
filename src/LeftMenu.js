eb522aec96c2d441ac2bfaa2ce24edd7a2355ae6
Andreas Bjuvefors <lb.andreas@gmail.com>
2025-10-09 11:45:07 +0200

    Updated news icon, now with correct time

--- src/LeftMenu.js
+++ src/LeftMenu.js
@@ -23,7 +23,7 @@ function LeftMenu({ userName, setUserName }) {
 
   // Manually update this timestamp when you want the changelog to be considered "updated".
   // Edit this constant in the source and deploy/build to change the value.
-  const MANUAL_LAST_UPDATED = "2025-10-13T00:00:00.000Z";
+  const MANUAL_LAST_UPDATED = "2025-10-08T00:00:00.000Z";
 
   const checkUnread = () => {
     try {
